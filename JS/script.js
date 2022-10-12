20714c8832f1527b81a3f191f75c6d6634fa8b5a
Mario Acendra <mariojoacre31@gmail.com>
2022-10-11 22:17:59 -0500

    creando repositorio

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+function reproducir() { 
+    var video1=document.getElementById('video1'); 
+    video1.play(); 
+    var video2=document.getElementById('video2'); 
+    video2.play(); 
+}
+
+function pausar(){
+    var video1=document.getElementById('video1'); 
+    video1.load(); 
+    var video2=document.getElementById('video2'); 
+    video2.load(); 
+}
