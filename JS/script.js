c488ff42e41e7491c8f6e32cfbd206efc592846c
Mario Acendra <mariojoacre31@gmail.com>
2023-07-03 17:34:27 -0500

    Update Script

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,11 @@
-function reproducir() { 
-    var video1=document.getElementById('video1'); 
-    video1.play(); 
-    var video2=document.getElementById('video2'); 
-    video2.play(); 
+function reproducir(video) { 
+    var nvideo=video;
+    var videop=document.getElementById(nvideo); 
+    videop.play(); 
 }
 
-function pausar(){
-    var video1=document.getElementById('video1'); 
-    video1.load(); 
-    var video2=document.getElementById('video2'); 
-    video2.load(); 
+function pausar(video){
+    var nvideo=video;
+    var videop=document.getElementById(nvideo); 
+    videop.load(); 
 }
